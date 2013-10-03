d6cbaf4d456e48e81f90139774387bd9f377e1f5
Striddle <Striddle@hotmail.com>
2013-10-03 08:40:41 -0400

    Started the game

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Think of a number between 1 and 100. I will try to guess it. AND I ALWAYS WIN!");
+            Console.WriteLine("Enter either h, for higher, l for less, and e, for even.");
+            Console.WriteLine("First Guess:50");
+
+            string input = Console.ReadLine();
+
+            
+
+            if (input == "e")
+            {
+                Console.WriteLine("I WIIIIIIIIIN!");
+            }
+
+            if (input == "h")
+            {
+                Console.WriteLine("");
+            }
+        }
+    }
+}
